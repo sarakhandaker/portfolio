ef11ee0a232e4dce5488028d70689df1a42e84e9
sarakhandaker <kttsara@gmail.com>
2020-10-23 13:35:30 -0700

    css for list added

--- src/components/About.js
+++ src/components/About.js
@@ -6,47 +6,46 @@ class About extends Component {
             <div className='about mt-3 mb-3'>
                 <h1>About Me:</h1>
                 <hr />
-                <div class="col-sm-12 text-center">
-                    <img alt="React" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" class="techlogo" width="150" />
-                    <img style={{ "marginRight": "30px" }} alt="Bootstrap" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" class="techlogo" width="80" />
-                    <img alt="PostgreSQL" src="//upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/220px-Postgresql_elephant.svg.png" class="techlogo" width="80" />
-                    <img alt="Postman" src="https://img.pngio.com/git-logo-png-5-png-image-git-png-180_180.png" class="techlogo" width="100" />
-                </div>
                 <div className='about-content mt-3 p-3 container'>
                     <div className="row">
-                        <div class="col medium-widget-article__description" style={{ "color": "black", "fontSize": "20px" }}>
+                        <div class="col-lg-8 medium-widget-article__description" style={{ "color": "black", "fontSize": "20px" }}>
                             <strong>Hello!</strong> <br /><br />
                             I'm Sara, a full stack software engineer based in Seattle, WA.<br /><br />
-                             I have a background in Civil/ Geotechnical Engineering and I enjoy creating things that help people interact with the city or spaces they live in.<br />
-                             My goal is to use my strong technical expertise to innovate software solutions for the urban environments of tomorrow.<br />
-                             I am passionate about sustainable environmentally friendly design with a keen insterest in public transit.<br /><br />
-                             I recently graduated from Flatiron School's Software Engineering Immersive Program.<br />
+                             I have a background in Civil/ Geotechnical Engineering and I enjoy helping people interact with the city/ spaces they live in.<br /><br />
+                             I have experience on large engineering projects focused on clients' needs to deliver quality products. My range of international work experience in Germany, Canada, Tunisia, and the USA offers me a unique and global perspective.<br /><br />
+                             My goal is to use my strong technical expertise to innovate software solutions for the smart urban environments of tomorrow.
+                             I am passionate about sustainable, environmental design with a keen insterest in public transit.<br /><br />
+                             I recently graduated from Flatiron School's Software Engineering Program.<br /><br />
+
+                            <img alt="ASP.NET" src="https://static.gunnarpeipman.com/wp-content/uploads/2020/09/aspnet-featured.png" class="techlogo" width="100" />
+                            <img style={{ "marginRight": "30px" }} alt="Bootstrap" src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/512px-Bootstrap_logo.svg.png" class="techlogo" width="55" />
+                            <img alt="PostgreSQL" src="//upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/220px-Postgresql_elephant.svg.png" class="techlogo" width="45" />
+                            <img alt="Git" style={{ "marginLeft": "10px", "marginRight": "-10px" }} src="https://img.pngio.com/git-logo-png-5-png-image-git-png-180_180.png" class="techlogo" width="65" />
+                            <img alt="React" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/512px-React-icon.svg.png" class="techlogo" width="110" />
+                            <img alt="Heroku" style={{ "marginRight": "30px" }} src="https://cdn.worldvectorlogo.com/logos/heroku.svg" class="techlogo" width="40" />
+                            <img alt="Ruby on Rails" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Ruby_On_Rails_Logo.svg/1200px-Ruby_On_Rails_Logo.svg.png" class="techlogo" width="110" />
+                            <img alt="Github" src="https://396241.smushcdn.com/1598754/wp-content/uploads/2020/03/postman-logo-vert-2018.jpg?lossy=1&strip=1&webp=1" class="techlogo" width="110" />
+
                         </div>
-                        <div class="col medium-widget-article__description" style={{ "color": "black", "fontSize": "20px" }}>
-                            <p><strong>Technologies I have been working with:</strong></p>
-                            <ul class="skills-list" style={{ "listStyle": "none" }}>
-                                <li>JavaScript</li>
-                                <li>Ruby</li>
-                                <li>C#</li>
-                                <li>SQL</li>
-                                <li>HTML &amp; CSS</li>
-                                <li>Bootstrap</li>
-                                <li>Ruby on Rails</li>
-                                <li>React</li>
-                                <li>ASP.NET</li>
-                                <li>Git</li>
-                                <li>Postman</li>
-                            </ul>
+                        <div class="col-lg-4 medium-widget-article__description" style={{ "color": "black", "fontSize": "20px" }}>
+                            <div class="list-type4">
+                                <ul class="skills-list" style={{ "listStyle": "none" }}>
+                                    <li>JavaScript</li>
+                                    <li>Ruby</li>
+                                    <li>C#</li>
+                                    <li>SQL</li>
+                                    <li>HTML &amp; CSS</li>
+                                    <li>Bootstrap</li>
+                                    <li>Ruby on Rails</li>
+                                    <li>React</li>
+                                    <li>ASP.NET</li>
+                                    <li>Git</li>
+                                    <li>Postman</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-12 text-center m-3">
-                    <img alt="ASP.NET" src="https://static.gunnarpeipman.com/wp-content/uploads/2020/09/aspnet-featured.png" class="techlogo" width="150" />
-                    <img alt="Heroku" style={{ "marginRight": "30px" }} src="https://cdn.worldvectorlogo.com/logos/heroku.svg" class="techlogo" width="60" />
-                    <img alt="Ruby on Rails" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Ruby_On_Rails_Logo.svg/1200px-Ruby_On_Rails_Logo.svg.png" class="techlogo" width="150" />
-                    <img alt="Github" src="https://396241.smushcdn.com/1598754/wp-content/uploads/2020/03/postman-logo-vert-2018.jpg?lossy=1&strip=1&webp=1" class="techlogo" width="140" />
-                </div>
-
             </div>
         );
     }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,97 +7,55 @@ class Navbar extends Component {
     };
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light main-navigation fixed-top mb-2">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                    <li className="nav-list-item"><Link className='link nav-item nav-link' activeClass="active" to="home" spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                        onClick={this.scrollToTop}
-                    >Home</Link></li>
-                    <li className="nav-list-item"><Link
-                        className='link nav-item nav-link'
-                        activeClass="active"
-                        to="About"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >About</Link></li>
-                    <li className="nav-list-item"><Link
-                        className='link nav-item nav-link'
-                        activeClass="active"
-                        to="Projects"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >Projects</Link></li>
-                    <li className="nav-list-item"><Link
-                        className='link nav-item nav-link'
-                        activeClass="active"
-                        to="Blog"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >Blog</Link></li>
+                <div class="collapse navbar-collapse mb-2" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto nav-list">
+                        <li className="nav-list-item"><Link className='link nav-item nav-link' activeClass="active" to="home" spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                            onClick={this.scrollToTop}
+                        >Home</Link></li>
+                        <li className="nav-list-item"><Link
+                            className='link nav-item nav-link'
+                            to="About"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >About</Link></li>
+                        <li className="nav-list-item"><Link
+                            className='link nav-item nav-link'
+                            to="Projects"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >Projects</Link></li>
+                        <li className="nav-list-item"><Link
+                            className='link nav-item nav-link'
+                            to="Blog"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration={500}
+                        >Blog</Link></li>
+                        
                     </ul>
-                    <li className="nav-list-item link nav-item nav-link">
-                        <span className="ml-3"> Contact: </span>
-                        <a href={`mailto:kttsara@gmail.com`}><i class="fa fa-envelope"></i></a>
-                        <a href="https://www.linkedin.com/in/sara-khandaker/"><i class="fa fa-linkedin"></i></a>
-                        <a href="https://github.com/sarakhandaker"><i class="fa fa-github"></i></a>
-                    </li>
+                    <div className="contact"> 
+                    <span style={{"marginRight": "10px"}}> Contact: </span>
+                    <a href={`mailto:kttsara@gmail.com`}><i class="fa fa-envelope"></i></a>
+                    <a href="https://www.linkedin.com/in/sara-khandaker/"><i class="fa fa-linkedin"></i></a>
+                    <a href="https://github.com/sarakhandaker"><i class="fa fa-github"></i></a>
+                    </div>
+                </div>
+                <div>
+
                 </div>
             </nav>
-                /* <nav className="main-navigation">
-                <ul className="nav-list">
-                    <li className="nav-list-item"><Link className='link nav-item nav-link' activeClass="active" to="home" spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                        onClick={this.scrollToTop}
-                    >Home</Link></li>
-                    <li className="nav-list-item"><Link
-                        className='link nav-item nav-link'
-                        activeClass="active"
-                        to="About"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >About</Link></li>
-                    <li className="nav-list-item"><Link
-                        className='link nav-item nav-link'
-                        activeClass="active"
-                        to="Projects"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >Projects</Link></li>
-                    <li className="nav-list-item"><Link
-                        className='link nav-item nav-link'
-                        activeClass="active"
-                        to="Blog"
-                        spy={true}
-                        smooth={true}
-                        offset={-70}
-                        duration={500}
-                    >Blog</Link></li>
-                    <li className="nav-list-item">
-                        <span className="mr-3"> Contact: </span>
-                        <a href={`mailto:kttsara@gmail.com`}><i class="fa fa-envelope"></i></a>
-                        <a href="https://www.linkedin.com/in/sara-khandaker/"><i class="fa fa-linkedin"></i></a>
-                        <a href="https://github.com/sarakhandaker"><i class="fa fa-github"></i></a>
-                    </li>
-                </ul>
-            </nav> */
         );
     }
 }
