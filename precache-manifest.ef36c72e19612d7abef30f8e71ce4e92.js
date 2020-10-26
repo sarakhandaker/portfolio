8b392c3a34e1b309ac87f1576e9d27c428116eec
sarakhandaker <kttsara@gmail.com>
2020-10-26 11:49:09 -0700

    Updates

---
+++ precache-manifest.ef36c72e19612d7abef30f8e71ce4e92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b3cb0a88d910382bbeb74adf03b157d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0027876428ccdaf34ac1",
+    "url": "/portfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "13318f5e5c4014699702",
+    "url": "/portfolio/static/css/main.d31f7431.chunk.css"
+  },
+  {
+    "revision": "0027876428ccdaf34ac1",
+    "url": "/portfolio/static/js/2.c8616d37.chunk.js"
+  },
+  {
+    "revision": "0b9d662f4c4008b1a5cf13593dfed097",
+    "url": "/portfolio/static/js/2.c8616d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13318f5e5c4014699702",
+    "url": "/portfolio/static/js/main.aa5a3e12.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0a7080d7f56a4fc27fbcfa93bad7a793",
+    "url": "/portfolio/static/media/KC_app.0a7080d7.png"
+  },
+  {
+    "revision": "ae103ffb54a29de8d61b0edbec5f5eaf",
+    "url": "/portfolio/static/media/techlogos.ae103ffb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.381d82e19bc677438f5785bb4be51f1b.js"
+  "/portfolio/precache-manifest.ef36c72e19612d7abef30f8e71ce4e92.js"
 );
 
 self.addEventListener('message', (event) => {
