fde3be448f988a5ccc1c2ceedeb41c33ed817d2c
sarakhandaker <kttsara@gmail.com>
2020-10-29 15:46:57 -0700

    fixed images for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,13 @@ class Header extends Component {
     render() {
         return (<div className="arrow-right">
             <header>
-            <img src="https://avatars3.githubusercontent.com/u/60558804?s=460&u=0282bcd2024786e7e00f2ae659c724c9b14524ce&v=4" className="img-circle" alt='about'></img>
+                <img src="https://avatars3.githubusercontent.com/u/60558804?s=460&u=0282bcd2024786e7e00f2ae659c724c9b14524ce&v=4" className="imgcircle" alt='about'></img>
                 <h1>Sara Khandaker</h1>
             </header>
             <p className='header-title'>
-            Full Stack Software Developer<br></br>
-            Innovating Solutions for Urban Environments
-               <br></br>
-                </p>
+                Full Stack Software Developer<br></br>
+                Innovating Solutions for Urban Environments<br></br>
+            </p>
         </div>);
     }
 }
