7b8ce5f00dc39ca8cde1efcb322cd72353cfa1d0
sarakhandaker <kttsara@gmail.com>
2021-02-17 10:37:44 -0800

    created reading component

---
+++ src/components/Read.js
@@ -0,0 +1,85 @@
+import React, { Component } from "react";
+
+
+export class Read extends Component {
+    state = {
+    };
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className='blog container mt-3 p-3'>
+                <div id="gr_custom_widget_1613586538">
+                    <div class="gr_custom_container_1613586538">
+                        <h2 class="gr_custom_header_1613586538">
+                            <a style="text-decoration: none;" rel="nofollow" href="https://www.goodreads.com/review/list/37327438-sara?shelf=read&amp;utm_medium=api&amp;utm_source=custom_widget">Recently Read</a>
+                        </h2>
+                        <div class="gr_custom_each_container_1613586538">
+                            <div class="gr_custom_book_container_1613586538">
+                                <a title="Gut: The Inside Story of Our Body’s Most Underrated Organ" rel="nofollow" href="https://www.goodreads.com/review/show/3843808330?utm_medium=api&amp;utm_source=custom_widget"><img alt="Gut: The Inside Story of Our Body’s Most Underrated Organ" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1435168687l/23013953._SY75_.jpg" /></a>
+                            </div>
+                            <div class="gr_custom_title_1613586538">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/3843808330?utm_medium=api&amp;utm_source=custom_widget">Gut: The Inside Story of Our Body’s Most Underrated Organ</a>
+                            </div>
+                            <div class="gr_custom_author_1613586538">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/7831553.Giulia_Enders">Giulia Enders</a>
+                            </div>
+                        </div>
+                        <div class="gr_custom_each_container_1613586538">
+                            <div class="gr_custom_book_container_1613586538">
+                                <a title="Farsighted: How We Make the Decisions That Matter the Most" rel="nofollow" href="https://www.goodreads.com/review/show/3843821168?utm_medium=api&amp;utm_source=custom_widget"><img alt="Farsighted: How We Make the Decisions That Matter the Most" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1523886240l/38769051._SY75_.jpg" /></a>
+                            </div>
+                            <div class="gr_custom_title_1613586538">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/3843821168?utm_medium=api&amp;utm_source=custom_widget">Farsighted: How We Make the Decisions That Matter the Most</a>
+                            </div>
+                            <div class="gr_custom_author_1613586538">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/1563.Steven_Johnson">Steven Johnson</a>
+                            </div>
+                        </div>
+                        <div class="gr_custom_each_container_1613586538">
+                            <div class="gr_custom_book_container_1613586538">
+                                <a title="Death of a Salesman" rel="nofollow" href="https://www.goodreads.com/review/show/1115674087?utm_medium=api&amp;utm_source=custom_widget"><img alt="Death of a Salesman" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1554514105l/12898._SY75_.jpg" /></a>
+                            </div>
+                            <div class="gr_custom_title_1613586538">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/1115674087?utm_medium=api&amp;utm_source=custom_widget">Death of a Salesman</a>
+                            </div>
+                            <div class="gr_custom_author_1613586538">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/8120.Arthur_Miller">Arthur  Miller</a>
+                            </div>
+                        </div>
+                        <div class="gr_custom_each_container_1613586538">
+                            <div class="gr_custom_book_container_1613586538">
+                                <a title="1984" rel="nofollow" href="https://www.goodreads.com/review/show/1115648138?utm_medium=api&amp;utm_source=custom_widget"><img alt="1984" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348990566l/5470._SY75_.jpg" /></a>
+                            </div>
+                            <div class="gr_custom_title_1613586538">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/1115648138?utm_medium=api&amp;utm_source=custom_widget">1984</a>
+                            </div>
+                            <div class="gr_custom_author_1613586538">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/3706.George_Orwell">George Orwell</a>
+                            </div>
+                        </div>
+                        <div class="gr_custom_each_container_1613586538">
+                            <div class="gr_custom_book_container_1613586538">
+                                <a title="The Great Gatsby" rel="nofollow" href="https://www.goodreads.com/review/show/1115648692?utm_medium=api&amp;utm_source=custom_widget"><img alt="The Great Gatsby" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1490528560l/4671._SY75_.jpg" /></a>
+                            </div>
+                            <div class="gr_custom_title_1613586538">
+                                <a rel="nofollow" href="https://www.goodreads.com/review/show/1115648692?utm_medium=api&amp;utm_source=custom_widget">The Great Gatsby</a>
+                            </div>
+                            <div class="gr_custom_author_1613586538">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/3190.F_Scott_Fitzgerald">F. Scott Fitzgerald</a>
+                            </div>
+                        </div>
+                        <br style="clear: both" />
+                        <center>
+                            <a rel="nofollow" href="https://www.goodreads.com/"><img alt="goodreads.com" style="border:0" src="https://s.gr-assets.com/images/widget/widget_logo.gif" /></a>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Read;
