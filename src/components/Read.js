286b768f4c0ae957b807bc3ea52f30c0070fe694
sarakhandaker <kttsara@gmail.com>
2021-06-07 15:59:56 -0700

    updated reading list

--- src/components/Read.js
+++ src/components/Read.js
@@ -15,17 +15,31 @@ export class Read extends Component {
                 <hr />
                 <div className="container mb-4 pb-4">
                     <h2>Currently Reading</h2>
+
                     <div class="row mt-3 p-3">
                         <div class="col-sm">
-                        <img alt="The Atlas of Unusual Borders: Discover Intriguing Boundaries, Territories and Geographical Curiosities" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1565781146l/45169008._SX200_.jpg" />
+                            <img alt="Algorithms to Live By: The Computer Science of Human Decisions" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1454296875l/25666050._SY200_.jpg" />
                         </div>
                         <div class="col-sm m-auto pt-3">
-                            <h4>The Atlas of Unusual Borders: Discover Intriguing Boundaries, Territories and Geographical Curiosities </h4>
-                            by <a rel="nofollow" href="https://www.goodreads.com/author/show/6907449.Zoran_Nikoli_">Zoran Nikolić</a>
+                            <h4>Algorithms to Live By: The Computer Science of Human Decisions </h4>
+                            by <a rel="nofollow" href="https://www.goodreads.com/author/show/4199891.Brian_Christian">Brian Christian</a>
                         </div>
                     </div>
+
                     <h2>Recently Read</h2>
+
                     <div class="row mt-3 p-3">
+                        <div class="col-sm pb-5">
+                            <div class="pb-3">
+                                <img alt="The Atlas of Unusual Borders: Discover Intriguing Boundaries, Territories and Geographical Curiosities" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1565781146l/45169008._SX100_.jpg" />
+                            </div>
+                            <div class="font-weight-bold">
+                                The Atlas of Unusual Borders: Discover Intriguing Boundaries, Territories and Geographical Curiosities
+                            </div>
+                            <div class="gr_custom_author_1613588504">
+                                by <a rel="nofollow" href="https://www.goodreads.com/author/show/6907449.Zoran_Nikoli_">Zoran Nikolić</a>
+                            </div>
+                        </div>
                         <div class="col-sm pb-5">
                             <div class="pb-3">
                                 <img alt="Range: Why Generalists Triumph in a Specialized World" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1550048292l/41795733._SX100_.jpg" />
@@ -48,7 +62,7 @@ export class Read extends Component {
                                 by <a rel="nofollow" href="https://www.goodreads.com/author/show/7831553.Giulia_Enders">Giulia Enders</a>
                             </div>
                         </div>
-                        <div class="col-sm pb-5">
+                        {/* <div class="col-sm pb-5">
                             <div class="pb-3">
                                 <img alt="The Atlas of Disease: Mapping Deadly Epidemics and Contagion from the Plague to the Zika Virus" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1534533036l/38509772._SX105_.jpg" />
                             </div>
@@ -58,7 +72,7 @@ export class Read extends Component {
                             <div class="gr_custom_author_1613588504">
                                 by <a rel="nofollow" href="https://www.goodreads.com/author/show/40102.Sandra_Hempel">Sandra Hempel</a>
                             </div>
-                        </div>
+                        </div> */}
                         {/* <div class="col-sm pb-5">
                             <div class="pb-3">
                                 <img alt="Farsighted: How We Make the Decisions That Matter the Most" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1523886240l/38769051._SY150
